85c39e2f2ad92a3a71b99977ecc170bbe2230854
esentai <esentaik@gmail.com>
2018-09-04 23:07:51 +0600

    contact me fixed

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,15 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'dauyelbekova@ieltsapai.kz'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'dauyelbekova@ieltsapai.kz, esentaik@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: dauyelbekova@ieltsapai.kz\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers = array(
+    'From' => 'dauyelbekova@ieltsapai.kz',
+    'Reply-To' => 'dauyelbekova@ieltsapai.kz',
+    'X-Mailer' => 'PHP/' . phpversion()
+); 
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
